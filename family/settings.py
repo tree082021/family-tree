3e9b83e251e52ef3152eb2e6795b951138185c53
unknown <service.tek.bot@mail.ru>
2022-12-04 11:57:46 +0600

    First commit

--- family/settings.py
+++ family/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = 'django-insecure-^@chv85^xzzvi+2x3k-hwzu9*zch5t%$4uxb$b_*fm31vb79mu
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG = True
+#DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'family-tree-h8wz.onrender.com/',]
+ALLOWED_HOSTS = ['127.0.0.1', 'family-tree-h8wz.onrender.com',]
 
 # Application definition (Зарегистрированные приложегия)
 
